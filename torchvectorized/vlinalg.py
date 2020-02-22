fd215ec34b904494bcf451b28b3a1ec15e681d53
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-02-22 13:07:31 -0500

    + Added none checkup on eigen vectors

--- torchvectorized/vlinalg.py
+++ torchvectorized/vlinalg.py
@@ -64,4 +64,4 @@ def vSymeig(input, eigen_vectors=False, flatten_output=False):
         eig_vals = eig_vals.permute(0, 2, 3, 4, 1).reshape(b * d * h * w, 3)
         eig_vecs = eig_vecs.permute(0, 3, 4, 5, 1, 2).reshape(b * d * h * w, 3, 3) if eigen_vectors else eig_vecs
 
-    return eig_vals.float(), eig_vecs.float()
+    return eig_vals.float(), eig_vecs.float() if eig_vecs is not None else None
