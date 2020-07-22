ee45df0a1bf75deedf2f31a78a88e37438eaf0f9
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-07-22 17:24:59 -0400

    + Added identity decomposition

--- tests/test_vlinalg.py
+++ tests/test_vlinalg.py
@@ -9,7 +9,7 @@ from torchvectorized.vlinalg import vSymEig, vExpm, vLogm
 class EigDecompositionTest(unittest.TestCase):
 
     def test_should_decompose_symmetric_matrices(self):
-        b, c, d, h, w = 16, 9, 32, 32, 32
+        b, c, d, h, w = 1, 9, 32, 32, 32
         input = self.sym(torch.rand(b, c, d, h, w))
         eig_vals, eig_vecs = vSymEig(input, eigen_vectors=True, flatten_output=True)
 
@@ -20,11 +20,32 @@ class EigDecompositionTest(unittest.TestCase):
         assert_that(torch.allclose(reconstructed_input, input, atol=0.000001), equal_to(True))
 
     def test_should_compute_matrices_exp_and_log(self):
-        b, c, d, h, w = 16, 9, 32, 32, 32
+        b, c, d, h, w = 1, 9, 32, 32, 32
         input = self.sym(torch.rand(b, c, d, h, w))
         reconstructed_input = vLogm(vExpm(input))
 
         assert_that(torch.allclose(reconstructed_input, input, atol=0.000001), equal_to(True))
+
+    def test_should_decompose_identity_matrix(self):
+        b, c, d, h, w = 2, 9, 1, 1, 1
+        input = self.sym(torch.zeros(b, c, d, h, w))
+        input[:, 0, :, :, :] = 1.0
+        input[:, 4, :, :, :] = 1.0
+        input[:, 8, :, :, :] = 1.0
+
+        eig_vals, eig_vecs = vSymEig(input, eigen_vectors=True, flatten_output=True)
+
+        assert_that(torch.allclose(eig_vals, torch.ones((2, 3)), atol=0.000001), equal_to(True))
+        assert_that(torch.allclose(eig_vecs, torch.eye(3).unsqueeze(0).repeat(2, 1, 1), atol=0.000001), equal_to(True))
+
+    def test_should_not_fail_on_empty_matrix(self):
+        b, c, d, h, w = 2, 9, 1, 1, 1
+        input = self.sym(torch.zeros(b, c, d, h, w))
+
+        eig_vals, eig_vecs = vSymEig(input, eigen_vectors=True, flatten_output=True)
+
+        assert_that(torch.allclose(eig_vals, torch.zeros((2, 3)), atol=0.000001), equal_to(True))
+        assert_that(torch.allclose(eig_vecs, torch.eye(3).unsqueeze(0).repeat(2, 1, 1), atol=0.000001), equal_to(True))
 
     def sym(self, inputs):
         return (inputs + inputs[:, [0, 3, 6, 1, 4, 7, 2, 5, 8], :, :, :]) / 2.0
--- torchvectorized/vlinalg.py
+++ torchvectorized/vlinalg.py
@@ -3,6 +3,15 @@ from math import pi
 import torch
 
 
+def _get_diag_matrices_index(input: torch.Tensor):
+    a12 = input[:, 1, :, :, :].double()
+    a13 = input[:, 2, :, :, :].double()
+    a23 = input[:, 5, :, :, :].double()
+    nd = torch.pow(a12, 2) + torch.pow(a13, 2) + torch.pow(a23, 2)
+
+    return torch.where(nd == 0)
+
+
 def _compute_eigen_values(input: torch.Tensor):
     b, c, d, h, w = input.size()
     a11 = input[:, 0, :, :, :].double()
@@ -15,25 +24,32 @@ def _compute_eigen_values(input: torch.Tensor):
 
     nd = torch.pow(a12, 2) + torch.pow(a13, 2) + torch.pow(a23, 2)
 
-    q = (a11 + a22 + a33) / 3.0
-    p = torch.pow((a11 - q), 2) + torch.pow((a22 - q), 2) + torch.pow((a33 - q), 2) + 2.0 * nd
-    p = torch.sqrt(p / 6.0)
+    if torch.any(nd != 0):
+        q = (a11 + a22 + a33) / 3.0
+        p = torch.pow((a11 - q), 2) + torch.pow((a22 - q), 2) + torch.pow((a33 - q), 2) + 2.0 * nd
+        p = torch.sqrt(p / 6.0)
 
-    r = torch.pow((1.0 / p), 3) * ((a11 - q) * ((a22 - q) * (a33 - q) - a23 * a23) - a12 * (
-            a12 * (a33 - q) - a13 * a23) + a13 * (a12 * a23 - a13 * (a22 - q))) / 2.0
+        r = torch.pow((1.0 / p), 3) * ((a11 - q) * ((a22 - q) * (a33 - q) - a23 * a23) - a12 * (
+                a12 * (a33 - q) - a13 * a23) + a13 * (a12 * a23 - a13 * (a22 - q))) / 2.0
 
-    phi = torch.acos(r) / 3.0
-    phi[r <= -1] = pi / 3
-    phi[r >= 1] = 0
+        phi = torch.acos(r) / 3.0
+        phi[r <= -1] = pi / 3
+        phi[r >= 1] = 0
 
-    eig_vals[:, 0, :, :, :] = q + 2 * p * torch.cos(phi)
-    eig_vals[:, 2, :, :, :] = q + 2 * p * torch.cos(phi + pi * (2.0 / 3.0))
-    eig_vals[:, 1, :, :, :] = 3 * q - eig_vals[:, 0, :, :, :] - eig_vals[:, 2, :, :, :]
+        eig_vals[:, 0, :, :, :] = q + 2 * p * torch.cos(phi)
+        eig_vals[:, 2, :, :, :] = q + 2 * p * torch.cos(phi + pi * (2.0 / 3.0))
+        eig_vals[:, 1, :, :, :] = 3 * q - eig_vals[:, 0, :, :, :] - eig_vals[:, 2, :, :, :]
+
+    if torch.any(nd == 0):
+        diag_matrix_index = torch.where(nd == 0)
+        eig_vals[:, 0, :, :, :][diag_matrix_index] = a11[diag_matrix_index]
+        eig_vals[:, 1, :, :, :][diag_matrix_index] = a22[diag_matrix_index]
+        eig_vals[:, 2, :, :, :][diag_matrix_index] = a33[diag_matrix_index]
 
     return eig_vals
 
 
-def _compute_eigen_vectors(input: torch.Tensor, eigen_values: torch.Tensor):
+def _compute_eigen_vectors(input: torch.Tensor, eigen_values: torch.Tensor, diag_index=None):
     a11 = input[:, 0, :, :, :].unsqueeze(1).expand(eigen_values.size()).double()
     a12 = input[:, 1, :, :, :].unsqueeze(1).expand(eigen_values.size()).double()
     a13 = input[:, 2, :, :, :].unsqueeze(1).expand(eigen_values.size()).double()
@@ -47,6 +63,14 @@ def _compute_eigen_vectors(input: torch.Tensor, eigen_values: torch.Tensor):
     u0 = u0 / norm
     u1 = u1 / norm
     u2 = u2 / norm
+
+    if torch.any(((eigen_values[:, 0, :, :, :] == eigen_values[:, 1, :, :, :]).float() * (
+            eigen_values[:, 0, :, :, :] == eigen_values[:, 2, :, :, :]).float()) == 1.0):
+        index = torch.where((eigen_values[:, 0, :, :, :] == eigen_values[:, 1, :, :, :]) == (
+                eigen_values[:, 0, :, :, :] == eigen_values[:, 2, :, :, :]))
+        u0[index[0], :, index[1], index[2], index[3]] = torch.tensor([1, 0, 0]).double()
+        u1[index[0], :, index[1], index[2], index[3]] = torch.tensor([0, 1, 0]).double()
+        u2[index[0], :, index[1], index[2], index[3]] = torch.tensor([0, 0, 1]).double()
 
     return torch.cat([u0.unsqueeze(1), u1.unsqueeze(1), u2.unsqueeze(1)], dim=1)
 
