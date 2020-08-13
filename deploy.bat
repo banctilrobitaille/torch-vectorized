a441134273cf18dfe7f181dc71287392d2da099c
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-08-13 13:00:12 -0400

    + Version up

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 python setup.py sdist
-python -m twine upload dist/torch-vectorized-0.1.12.tar.gz
+python -m twine upload dist/torch-vectorized-0.1.13.tar.gz
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 python3 setup.py sdist
-python3 -m twine upload dist/torch-vectorized-0.1.12.tar.gz
+python3 -m twine upload dist/torch-vectorized-0.1.13.tar.gz
