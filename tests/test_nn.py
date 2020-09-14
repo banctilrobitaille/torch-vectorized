+ Added tests and fixed FA gradients

tests/test_nn.py
@@ -3,12 +3,26 @@ import unittest
 import torch
 from hamcrest import assert_that, equal_to
 
-from torchvectorized.nn import EigVals
+from torchvectorized.debug.nn import Logm, EigVals, Expm
 from torchvectorized.utils import sym
 from torchvectorized.vlinalg import vSymEig
 
 
 class NnTest(unittest.TestCase):
+
+    def setUp(self):
+        self._target = torch.tensor(
+            [6.7756e-04, 3.4127e-05, -1.1893e-04, 3.4127e-05, 7.4100e-04, 9.1343e-05, -1.1893e-04, 9.1343e-05,
+             .7955e-04]).reshape(1, 9, 1, 1, 1)
+        self._target_log = torch.tensor(
+            [-7.31479692, 0.06039926, -0.18166597, 0.06039926, -7.21763706, 0.13518471, -0.18166597, 0.13518471,
+             -7.31935453]).reshape(1, 9, 1, 1, 1)
+        self._pred = torch.tensor(
+            [6.3381e-04, -1.0740e-04, -1.1193e-05, -1.0740e-04, 5.1552e-04, 2.0420e-05, -1.1193e-05, 2.0420e-05,
+             2.8239e-04]).reshape(1, 9, 1, 1, 1)
+        self._pred_log = torch.tensor(
+            [-7.38063335, - 0.1894127, - 0.02080916, - 0.1894127, - 7.59057522, 0.05062385, - 0.02080916, 0.05062385,
+             - 8.17417812]).reshape(1, 9, 1, 1, 1)
 
     def compute_fa(self, eigen_values: torch.Tensor):
         eig_1 = eigen_values[:, 0]
@@ -21,24 +35,17 @@ class NnTest(unittest.TestCase):
         return torch.clamp(torch.sqrt((num / (denom + 1e-15) + 1e-15)), 0, 1)
 
     def test_should_backward_eig_vals(self):
-        real_log = torch.tensor(
-            [-7.31479692, 0.06039926, - 0.18166597, 0.06039926, - 7.21763706, 0.13518471, - 0.18166597, 0.13518471,
-             - 7.31935453]).reshape(1, 9, 1, 1, 1)
-        fake_log = torch.tensor(
-            [-7.38063335, - 0.1894127, - 0.02080916, - 0.1894127, - 7.59057522, 0.05062385, - 0.02080916, 0.05062385,
-             - 8.17417812]).reshape(1, 9, 1, 1, 1)
-
-        real_log.requires_grad = False
-        fake_log.requires_grad = True
+        self._target_log.requires_grad = False
+        self._pred_log.requires_grad = True
 
         loss_fn = torch.nn.L1Loss()
         to_eig_vals = EigVals()
 
-        eig_1 = to_eig_vals(fake_log)
-        eig_2 = to_eig_vals(real_log)
+        eig_1 = to_eig_vals(self._pred_log)
+        eig_2 = to_eig_vals(self._target_log)
 
-        eig_1_torch, vec_1 = fake_log.reshape(1, 3, 3).symeig(eigenvectors=True)
-        eig_2_torch, vec_2 = real_log.reshape(1, 3, 3).symeig(eigenvectors=True)
+        eig_1_torch, vec_1 = self._pred_log.reshape(1, 3, 3).symeig(eigenvectors=True)
+        eig_2_torch, vec_2 = self._target_log.reshape(1, 3, 3).symeig(eigenvectors=True)
 
         fa_1 = self.compute_fa(torch.exp(eig_1))
         fa_2 = self.compute_fa(torch.exp(eig_2))
@@ -53,9 +60,64 @@ class NnTest(unittest.TestCase):
         loss_torch = loss_fn(fa_1_torch, fa_2_torch)
 
         loss.backward()
-        grad = fake_log.grad
+        grad = self._pred_log.grad.clone()
+        self._pred_log.grad = None
         loss_torch.backward()
-        grad_torch = fake_log.grad
+        grad_torch = self._pred_log.grad
+
+        assert_that(torch.allclose(grad, grad_torch, atol=0.000001), equal_to(True))
+
+    def test_should_backward_logm(self):
+        self._target_log.requires_grad = False
+        self._pred.requires_grad = True
+
+        loss_fn = torch.nn.L1Loss()
+        logm = Logm()
+
+        pred_log = logm(self._pred)
+        S, U = self._pred.reshape(3, 3).symeig(eigenvectors=True)
+
+        pred_log_torch = U.reshape(1, 3, 3).bmm(torch.diag_embed(torch.log(S.reshape(1, 3)))).bmm(
+            U.reshape(1, 3, 3).transpose(1, 2)).reshape(1, 9, 1, 1, 1)
+
+        assert_that(torch.allclose(pred_log_torch, self._pred_log, atol=0.00001), equal_to(True))
+        assert_that(torch.allclose(pred_log, self._pred_log, atol=0.00001), equal_to(True))
+
+        loss = loss_fn(pred_log, self._target_log)
+        loss_torch = loss_fn(pred_log_torch, self._target_log)
+
+        loss.backward()
+        grad = self._pred.grad.clone()
+        self._pred.grad = None
+        loss_torch.backward()
+        grad_torch = self._pred.grad
+
+        assert_that(torch.allclose(grad, grad_torch, atol=0.000001), equal_to(True))
+
+    def test_should_backward_expm(self):
+        self._target.requires_grad = False
+        self._pred_log.requires_grad = True
+
+        loss_fn = torch.nn.L1Loss()
+        expm = Expm()
+
+        pred = expm(self._pred_log)
+        S, U = self._pred_log.reshape(3, 3).symeig(eigenvectors=True)
+
+        pred_torch = U.reshape(1, 3, 3).bmm(torch.diag_embed(torch.exp(S.reshape(1, 3)))).bmm(
+            U.reshape(1, 3, 3).transpose(1, 2)).reshape(1, 9, 1, 1, 1)
+
+        assert_that(torch.allclose(pred_torch, self._pred, atol=0.00001), equal_to(True))
+        assert_that(torch.allclose(pred, self._pred, atol=0.00001), equal_to(True))
+
+        loss = loss_fn(pred, self._target)
+        loss_torch = loss_fn(pred_torch, self._target)
+
+        loss.backward()
+        grad = self._pred_log.grad.clone()
+        self._pred_log.grad = None
+        loss_torch.backward()
+        grad_torch = self._pred_log.grad
 
         assert_that(torch.allclose(grad, grad_torch, atol=0.000001), equal_to(True))
 

torchvectorized/debug/nn.py
@@ -75,7 +75,7 @@ class LogmFunc(torch.autograd.Function):
         # Backward Log
         inv_S = torch.diag_embed(1 / S)
         grad_U = 2 * _grad_sym(grad_X).bmm(U.bmm(torch.diag_embed(S_log)))
-        grad_S = torch.eye(3).cuda() * (inv_S.bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U))))
+        grad_S = torch.eye(3).to(grad_X.device) * (inv_S.bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U))))
 
         S = S.view(1, -1)
         P = S.view(S.size(1) // 3, 3).unsqueeze(2)
@@ -133,7 +133,8 @@ class ExpmFunc(torch.autograd.Function):
 
         grad_X = grad_outputs[0].reshape(b, 3, 3, d * h * w).permute(0, 3, 1, 2).reshape(b * d * h * w, 3, 3)
         grad_U = 2 * _grad_sym(grad_X).bmm(U.bmm(torch.diag_embed(S_exp)))
-        grad_S = torch.eye(3).cuda() * torch.diag_embed(S_exp).bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U)))
+        grad_S = torch.eye(3).to(grad_X.device) * torch.diag_embed(S_exp).bmm(
+            U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U)))
 
         S = S.view(1, -1)
         P = S.view(S.size(1) // 3, 3).unsqueeze(2)
@@ -194,7 +195,7 @@ class ExpmLogmFunc(torch.autograd.Function):
         # Backward Log
         inv_S = torch.diag_embed(1 / S_exp)
         grad_U = 2 * _grad_sym(grad_X).bmm(U.bmm(torch.diag_embed(S_log)))
-        grad_S = torch.eye(3).cuda() * (inv_S.bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U))))
+        grad_S = torch.eye(3).to(grad_X.device) * (inv_S.bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U))))
 
         S = S_exp.view(1, -1)
         P = S.view(S.size(1) // 3, 3).unsqueeze(2)
@@ -208,7 +209,8 @@ class ExpmLogmFunc(torch.autograd.Function):
 
         # Backward Exp
         grad_U = 2 * _grad_sym(grad_X).bmm(U.bmm(torch.diag_embed(S_exp)))
-        grad_S = torch.eye(3).cuda() * torch.diag_embed(S_exp).bmm(U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U)))
+        grad_S = torch.eye(3).to(grad_X.device) * torch.diag_embed(S_exp).bmm(
+            U.transpose(1, 2).bmm(_grad_sym(grad_X).bmm(U)))
 
         S = S_log.view(1, -1)
         P = S.view(S.size(1) // 3, 3).unsqueeze(2)