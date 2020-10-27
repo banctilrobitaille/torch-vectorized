d1121881402674cf88747ef7fe263785950d9ba9
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-10-27 09:38:14 -0400

    + Added performance results

--- tests/performance.py
+++ tests/performance.py
@@ -13,50 +13,64 @@ def rand_torch_input(shape):
 
 
 def vectorized_func(b):
-    c, d, h, w = 9, 32, 32, 32
+    c, d, h, w = 9, 10, 10, 10
     return timeit.timeit(lambda: vSymEig(sym(torch.rand(b, c, d, h, w)), eigenvectors=True), number=5) / (5 * 1000000)
 
 
 def torch_func(b):
-    c, d, h, w = 9, 32, 32, 32
-    return timeit.timeit(lambda: torch.symeig(rand_torch_input((b, c, d, h, w)), eigenvectors=True), number=5) / (
-            5 * 1000000)
+    c, d, h, w = 9, 10, 10, 10
+    return timeit.timeit(lambda: torch.symeig(rand_torch_input((b, c, d, h, w)), eigenvectors=True), number=5) / 5
 
 
 def vectorized_func_gpu(b):
-    c, d, h, w = 9, 32, 32, 32
-    return timeit.timeit(lambda: vSymEig(sym(torch.rand(b, c, d, h, w)).cuda(), eigenvectors=True), number=5) / (
-            5 * 1000000)
+    c, d, h, w = 9, 10, 10, 10
+    return timeit.timeit(lambda: vSymEig(sym(torch.rand(b, c, d, h, w)).cuda(), eigenvectors=True), number=5) / 5
 
 
 def torch_func_gpu(b):
-    c, d, h, w = 9, 32, 32, 32
+    c, d, h, w = 9, 10, 10, 10
     return timeit.timeit(lambda: torch.symeig(rand_torch_input((b, c, d, h, w)).cuda(), eigenvectors=True),
-                         number=5) / (5 * 1000000)
+                         number=5) / 5
 
 
 if __name__ == "__main__":
-    batch_sizes = range(0, 48)
+    batch_sizes = range(1, 500)
 
-    y1 = [vectorized_func(batch_size) for batch_size in batch_sizes]
+    """y1 = [vectorized_func(batch_size) for batch_size in batch_sizes]
     y2 = [torch_func(batch_size) for batch_size in batch_sizes]
 
-    plt.plot(batch_sizes, y1, label="Torch Vectorized")
-    plt.plot(batch_sizes, y2, label="Torch")
+    plt.plot([batch_size * 1000 for batch_size in batch_sizes], y1, label="Torch Vectorized")
+    plt.plot([batch_size * 1000 for batch_size in batch_sizes], y2, label="Torch")
     plt.xlabel('Batch size')
     plt.ylabel('Execution time (\u03BCs)')
-    plt.title('SymEig exec. time (\u03BCs) vs batch size (CPU)')
+    plt.title('SymEig exec. time (\u03BCs) vs batch size (K) (CPU)')
     plt.legend()
     plt.savefig("cpu_performance.png")
+    plt.close()
 
     if torch.cuda.is_available():
         y1 = [vectorized_func_gpu(batch_size) for batch_size in batch_sizes]
         y2 = [torch_func_gpu(batch_size) for batch_size in batch_sizes]
 
-        plt.plot(batch_sizes, y1, label="Torch Vectorized")
-        plt.plot(batch_sizes, y2, label="Torch")
+        plt.plot([batch_size * 1000 for batch_size in batch_sizes], y1, label="Torch Vectorized")
+        plt.plot([batch_size * 1000 for batch_size in batch_sizes], y2, label="Torch")
         plt.xlabel('Batch size')
         plt.ylabel('Execution time (\u03BCs)')
-        plt.title('SymEig exec. time (\u03BCs) vs batch size (GPU)')
+        plt.title('SymEig exec. time (\u03BCs) vs batch size (K) (GPU)')
         plt.legend()
         plt.savefig("gpu_performance.png")
+        plt.close()"""
+
+    if torch.cuda.is_available():
+        y1 = [vectorized_func(batch_size) for batch_size in batch_sizes]
+        y2 = [vectorized_func_gpu(batch_size) for batch_size in batch_sizes]
+
+        plt.plot([batch_size * 1000 for batch_size in batch_sizes], y1, label="Torch Vectorized (CPU)")
+        plt.plot([batch_size * 1000 for batch_size in batch_sizes], y2, label="Torch Vectorized (GPU)")
+        plt.xlabel('Batch size')
+        plt.ylabel('Execution time (ms)')
+        plt.title('Exec. time vs amount of eigendecomposition (GPU)')
+        plt.legend()
+        plt.savefig("performance_vectorized.png")
+        plt.close()
+
